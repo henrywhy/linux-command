9d7275e7cc38d2ab2d3e0c3012c9e46424db0073
Hongyuan Wang <710136012@qq.com>
2015-05-31 22:44:24 +0800

    read login information from binary file utmp and show it

---
+++ who/who.c
@@ -0,0 +1,36 @@
+/*
+ *show current system's login information,
+ *utmp file stores data, which is a sequence of utmp records
+ */
+#include <stdio.h>
+#include <utmp.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void show_record(struct utmp * p);
+
+int main(int argc, char * argv[])
+{
+	struct utmp cur_record;
+	int buffer_size = sizeof(cur_record);
+	int fd; //file descriptor
+
+	fd = open("/var/run/utmp", O_RDONLY);	
+	if(fd == -1) {
+		printf("open file error\n");
+		return 1;
+	}
+
+	while(read(fd, &cur_record, buffer_size)) {
+		show_record(&cur_record);	
+	}
+
+	//must close the utmp file
+	close(fd);
+}
+
+void show_record(struct utmp * p)
+{
+	puts(p->ut_user);
+}
+
