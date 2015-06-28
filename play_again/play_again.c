acbe5a251eb6f9e5bc2429b3dc56eb8922a6804e
Hongyuan Wang <710136012@qq.com>
2015-06-28 20:56:07 +0800

    play_agin version 0

--- play_again/play_again.c
+++ play_again/play_again.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+//0->yes  1->no
+int get_response(const char * question);
+
 int main(int argc, char * argv[])
 {
-	return 0;
+	char question[] = "play again?";
+	int response;
+	response = get_response(question);
+	return response;
+}
+
+int get_response(const char * question)
+{
+	printf("%s (y/n)", question);
+	while(1) {
+		switch(getchar()) {
+			case 'y' :
+			case 'Y' :
+				return 0;
+			case 'n' :
+			case 'N' :
+			case EOF :
+				return 1;
+		}
+	}	
 }
