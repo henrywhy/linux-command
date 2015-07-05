96b6d30656b069f9d2021eec5bfb65747a86bd85
Hongyuan Wang <710136012@qq.com>
2015-07-05 19:49:53 +0800

    handle ctrl-c interrupte signal

--- play_again/play_again.c
+++ play_again/play_again.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <termios.h>
+#include <signal.h>
 
 //0->yes  1->no
 int get_response(const char * question);
+static struct termios original_setting;
 
 int main(int argc, char * argv[])
 {
-	struct termios original_setting, tmp_setting;
+	struct termios tmp_setting;
 	tcgetattr(0, &original_setting);
 	tcgetattr(0, &tmp_setting);
 	tmp_setting.c_lflag &= ~ICANON;
 	tmp_setting.c_lflag &= ~ECHO;
 	tmp_setting.c_cc[VMIN] = 1;
 	tcsetattr(0, TCSANOW, &tmp_setting);
+
+	//handle signal ctrl-c
+	void user_exit(int);
+	signal(SIGINT, user_exit);	
 	
 	char question[] = "play again?";
 	int response;
@@ -37,4 +43,11 @@ int get_response(const char * question)
 				return 1;
 		}
 	}	
+}
+
+void user_exit(int signal_num)
+{
+	tcsetattr(0, TCSANOW, &original_setting);
+	printf("\n1\n");
+	exit(1);
 }
